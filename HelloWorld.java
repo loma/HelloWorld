da1b7e2890dcaa9bfe064dd5f75bdc9995fd14bc
loma <57.loma@gmail.com>
2018-07-03 17:41:59 +0700

    remove while loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,9 +5,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String name = sc.next();
-    while (!name.equals("0")) {
-      System.out.println("Hello, " + name);
-      name = sc.next();
-    }
+    System.out.println("Hello, " + name);
   }
 }
